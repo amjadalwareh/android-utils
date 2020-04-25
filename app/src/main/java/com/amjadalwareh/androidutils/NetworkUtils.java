a99850dd8aa42c5ec89542b9039c78eb33e13eb2
Amjad <a.alwareh25@gmail.com>
2020-04-25 18:16:27 +0300

    Modify isConnected

--- app/src/main/java/com/amjadalwareh/androidutils/NetworkUtils.java
+++ app/src/main/java/com/amjadalwareh/androidutils/NetworkUtils.java
@@ -1,6 +1,5 @@
 package com.amjadalwareh.androidutils;
 
-import android.Manifest;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -12,16 +11,17 @@ public final class NetworkUtils {
 
     /***
      * A method to check if device connected to network or not
-     * @param context
+     * @param context The app context
      * @return true if connected, otherwise false
      */
-    @RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
+    @RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
     public static boolean isNetworkConnected(@NonNull Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) Utils.getService(context, Context.CONNECTIVITY_SERVICE);
 
         if (connectivityManager == null) return false;
 
         NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+
         return activeNetwork != null && activeNetwork.isConnected();
     }
 
