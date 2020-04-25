13c180c11057622395c3c08463371e9c347d4efc
Amjad <a.alwareh25@gmail.com>
2020-04-25 18:03:04 +0300

    Bug fixes

--- app/src/main/java/com/amjadalwareh/androidutils/NetworkUtils.java
+++ app/src/main/java/com/amjadalwareh/androidutils/NetworkUtils.java
@@ -1,4 +1,28 @@
 package com.amjadalwareh.androidutils;
 
+import android.Manifest;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresPermission;
+
 public final class NetworkUtils {
+
+    /***
+     * A method to check if device connected to network or not
+     * @param context
+     * @return true if connected, otherwise false
+     */
+    //@RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
+    public static boolean isNetworkConnected(@NonNull Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) Utils.getService(context, Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager == null) return false;
+
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnected();
+    }
+
 }
